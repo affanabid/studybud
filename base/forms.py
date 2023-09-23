da90fdcdb7fae7f5f997bafbcabece6934ae8d7a
Harry <affan.abid99@gmail.com>
2023-09-23 16:19:58 +0500

    static files complete

--- base/forms.py
+++ base/forms.py
@@ -4,4 +4,6 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
+        # either add list of fields you want to add or just use '__all__' and then exclude the specefic ones.
         fields = '__all__'
+        exclude = ['host', 'participants']
