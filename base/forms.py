f66cedf10027ee8dcae2ec95487ed13f538d11e4
Harry <affan.abid99@gmail.com>
2023-10-22 18:04:45 +0500

    (5:24)

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -7,3 +8,9 @@ class RoomForm(ModelForm):
         # either add list of fields you want to add or just use '__all__' and then exclude the specefic ones.
         fields = '__all__'
         exclude = ['host', 'participants']
+
+class UpdateForm(ModelForm):
+    class Meta:
+        model = User
+        # either add list of fields you want to add or just use '__all__' and then exclude the specefic ones.
+        fields = ['username', 'email']
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.models import User
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UpdateForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -157,4 +157,15 @@ def delete_message(request, pk):
 
 @login_required(login_url=login)
 def update_user(request):
-    return render(request, 'base/update_user.html')
+    user = request.user
+    form = UpdateForm(instance=user)
+    if request.method == 'POST':
+        form = UpdateForm(request.POST, instance=user)
+        
+        if form.is_valid():
+
+            print("Form is valid!")
+            form.save()
+            print("Form is saved!")
+            return redirect('user_profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form': form})
